6d5ee94f25f3b68b3a8c072975f3ea617c47031f
ShivalilaPatil <shivalilapatil66@gmail.com>
2022-04-18 02:05:07 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { UsersdataService } from './services/usersdata.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app6';
